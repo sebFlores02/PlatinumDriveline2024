1cdc3753c02dff60b7fcaf36250370be8cdddfee
FloresTM <seb.flores2002@gmail.com>
2024-07-14 16:26:07 -0600

    favicon

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    domains: ["platinumdriveline.mx"],
+  },
+};
 
 export default nextConfig;
